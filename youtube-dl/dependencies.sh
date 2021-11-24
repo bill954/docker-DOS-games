66d639c8f759c2524fa26380fd40bb8a2c83bab3
bill954 <raafaa9573@gmail>
2021-11-24 18:09:12 -0300

    Modified the index.html file in all the games.

--- youtube-dl/dependencies.sh
+++
@@ -1,5 +0,0 @@
-apt-get update
-apt-get -y install wget
-apt-get -y install python
-wget https://yt-dl.org/downloads/latest/youtube-dl -O /usr/local/bin/youtube-dl
-chmod a+rx /usr/local/bin/youtube-dl
